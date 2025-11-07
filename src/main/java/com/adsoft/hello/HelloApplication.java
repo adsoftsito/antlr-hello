5f32673408784a666847c9209f749037e49ab29f
adsoftsito <adsoft@live.com.mx>
2025-11-06 18:44:39 -0600

    add antlr-hello

---
+++ src/main/java/com/adsoft/hello/HelloApplication.java
@@ -0,0 +1,13 @@
+package com.adsoft.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adsoft/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adsoft.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
